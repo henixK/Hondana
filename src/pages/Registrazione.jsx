8090fe2ef17c4c1b16c68268984fcd2a751f64df
Prudolo <prudenzino@gmail.com>
2024-05-22 16:48:25 +0200

    aggiornamento node , aggiunta registrazione pussy

---
+++ src/pages/Registrazione.jsx
@@ -0,0 +1,15 @@
+export default function Registrazione(){
+    return(
+        <div>
+            <h1>Form Esempio</h1>
+            <form method="post" action="esempio.php"></form>
+            <label for="name">Full Name</label>
+            <input type="text" id="Full Name" name="Full Name" placeholder="Insert Full Name"/>
+            <label for="cognome">Email</label>
+            <input type="text" id="cognome" name="cognome" placeholder="Inserisci cognome"/>
+            <label for=""></label>
+            <input type="submit" value="Invia Dati"/>
+        </div>
+        
+    )
+}
