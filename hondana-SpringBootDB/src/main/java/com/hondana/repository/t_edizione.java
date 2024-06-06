ad3c0253cc87842e5c6ee3d092f7bcd2230f1f1c
Loky17 <122970427+Loky17@users.noreply.github.com>
2024-06-06 11:39:10 +0200

    .

--- hondana-SpringBootDB/src/main/java/com/hondana/repository/t_edizione.java
+++ hondana-SpringBootDB/src/main/java/com/hondana/repository/t_edizione.java
@@ -8,8 +8,6 @@ import jakarta.persistence.Column;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
