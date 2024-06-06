3b619128f5b62d9e9987927b057a07f1b4aef99f
Loky17 <122970427+Loky17@users.noreply.github.com>
2024-06-06 17:40:46 +0200

    .

--- Backend/src/main/java/com/hondana/repository/hondanaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.hondana.repository;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository("prova")
-public interface hondanaRepository extends CrudRepository<t_utente, Integer>{
-
-}
